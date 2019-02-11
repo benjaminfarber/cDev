e2613b7c1ee753a028d8981c6edb7ce98523c007
benjaminfarber <benjaminefarber@gmail.com>
2019-02-11 13:53:08 -0500

    data input for story.c completed

--- story.c
+++ story.c
@@ -12,9 +12,8 @@
 
 // BEGIN FUNCTION PROTOTYPES
 
-char* capitalize(char *);
-char* uncapitalize(char *);
-int generateRandomNumber(void);
+int generateRandomNumber(int);
+void pause(int);
 
 // END FUNCTION PROTOTYPES
 
@@ -25,38 +24,108 @@ int main() {
     printf("\nWelcome to the story telling program!\n\n");
     printf("You will be presented with a series of questions\n");
     printf("and a story will be generated from your input.\n\n");
+    printf("-------------------------------------------------\n\n");
     
-    int storyGenerator = generateRandomNumber();
-    switch(storyGenerator) {
-        case 1:
-            printf("\nThe Grocery Store, by Ben Farber and %s", capitalize(yourName));
-            break;
-        case 2:
-            printf("\nThe Road Trip, by Ben Farber and %s", capitalize(yourName));
-            break;
-        case 3:
-            printf("\nThe Big Test, by Ben Farber and %s", capitalize(yourName));
-    }
+    char yourFirstName[25] = {'\0'};
+    printf("What is your first name? ");
+    scanf("%s", yourFirstName);
     
+    char hometown[25] = {'\0'};
+    printf("What city are you from? ");
+    scanf("%s", hometown);
+
+    char favoriteColor[25] = {'\0'};
+    printf("What is your favorite color? ");
+    scanf("%s", favoriteColor);
+
+    char favoriteFood[25] = {'\0'};
+    printf("What is your favorite food? ");
+    scanf("%s", favoriteFood);
+
+    char personalDescription1[25] = {'\0'};
+    printf("What is an adjective you would use to describe yourself? ");
+    scanf("%s", personalDescription1);
+
+    char personalDescription2[25] = {'\0'};
+    printf("What is another adjective you would use to describe yourself? ");
+    scanf("%s", personalDescription2);
+
+    char petName[25] = {'\0'};
+    printf("What is your pet's name? ");
+    scanf("%s", petName);
+
+    char lastFood[25] = {'\0'};
+    printf("What is the last thing you ate? ");
+    scanf("%s", lastFood);
+
+    char favoritePlace[25] = {'\0'};
+    printf("What is your favorite place? ");
+    scanf("%s", favoritePlace);
+
+    char yourJob[25] = {'\0'};
+    printf("What is your job? ");
+    scanf("%s", yourJob);
+
+    char yourDreamJob[25] = {'\0'};
+    printf("What is your dream job? ");
+    scanf("%s", yourDreamJob);
+
+    char grossFood[25] = {'\0'};
+    printf("What is the worst thing you've ever eaten? ");
+    scanf("%s", grossFood);
+
+    char bestFriend[25] = {'\0'};
+    printf("Who is your best friend? ");
+    scanf("%s", bestFriend);
+
+    char parentJob[25] = {'\0'};
+    printf("What is one of your parent's job? ");
+    scanf("%s", parentJob);
+
+    char vacation[25] = {'\0'};
+    printf("Where is the last place you went on vacation to? ");
+    scanf("%s", vacation);
+
+    char *names[3] = {yourFirstName, petName, bestFriend};
+    
+    char *foods[5] = {favoriteFood, lastFood, grossFood};
+
+    char *adjectives[3] = {favoriteColor, personalDescription1, personalDescription2};
+
+    char *places[3] = {hometown, favoritePlace, vacation};
+
+    char *job[3] = {yourJob, yourDreamJob, parentJob};
+
+    pause(2);
+
+    printf("Generating Story...\n");
+
+    pause(2);
+
+    system("clear");
+
+    printf("The story...\n");
+    printf("-------------------------------------------------\n\n");
+
+    return 0;
 }
 
 // END MAIN FUNCTION
 
 // BEGIN FUNCTION DEFINITIONS
 
-char* capitalize(char *str) {
-    str[0] = toupper(str[0]);
-    return str;
-}
-
-char* uncapitalize(char *str) {
-    str[0] = tolower(str[0]);
-    return str;
-}
-
-int generateRandomNumber(void) {
+int generateRandomNumber(int largestNumber) {
     srand(time(NULL));
-    return (rand() % 3) + 1;
+    return (rand() % largestNumber) + 1;
+}
+
+void pause(int waitTime) {
+    int currentTime = 0;
+    int passedTime = 0;
+    currentTime = time( NULL);
+    do {
+        passedTime = time( NULL);
+    } while ((passedTime - currentTime) < waitTime);
 }
 
 // END FUNCTION DEFINITIONS
