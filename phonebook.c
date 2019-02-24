74ae808aa46e3e9771024aca61e21a1c13a33c6a
benjaminfarber <benjaminefarber@gmail.com>
2019-02-24 12:28:02 -0500

    updated phonebook.c

--- phonebook.c
+++ phonebook.c
@@ -1,56 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-void createContact(int *);
-void deleteContact(int *);
-void displayContactList(int);
+#include <ctype.h>
 
 typedef struct contact {
-    char firstName[20];
-    char lastName[20];
-    char phoneNumber[15];
+    char firstName[30];
+    char lastName[30];
+    char phoneNumber[30];
 } contact;
 
+void createContact(contact[], int *);
+void deleteContact(contact[], int *);
+void displayContactList(contact[], int *);
+void getString(char *, int);
+char* capitalize(char *);
+
 int main(void) {
-    int numberOfContacts = 0;
-    int *pnumberOfContacts = &numberOfContacts;
+    int contactIndex = 1;
+
+    contact *contactList = (contact*)calloc(sizeof(contactList), 1);
+
     int action = 0;
+
     do {
         printf("\n-----Contacts-----\n");
         printf("(1) Create contact\n");
         printf("(2) Delete contact\n");
         printf("(3) View contacts\n");
         printf("(4) Exit Contacts\n\n");
-        printf("What would you like to do? ");
+        printf(">> ");
         scanf("%d", &action);
         system("clear");
         switch(action) {
             case 1:
                 printf("\nCreate Contact\n");
+                contactIndex++;
+                contactList=(contact*)realloc(contactList,sizeof(contact)*(contactIndex));
+	            if (contactList == NULL){
+		            printf("Could not allocate memory.");
+	            }                
+                createContact(contactList, &contactIndex);
+                printf("\ncontactIndex = %d", contactIndex);
                 break;
-            case 2;
+            case 2:
                 printf("\nDelete Contact\n");
+                contactIndex--;
+                deleteContact(contactList, &contactIndex);
+                printf("\ncontactIndex = %d", contactIndex);
                 break;
-            case 3;
+            case 3:
                 printf("\nYour Contacts\n");
+                displayContactList(contactList, &contactIndex);
+                printf("\ncontactIndex = %d", contactIndex);
                 break;
-            case 4;
-                printf("Exiting Contacts . . .");
+            case 4:
+                printf("Exiting Contacts . . .\n\n");
+                break;
+            default:
+                printf("That input is not valid.");
                 break;
         }
-    } while (action != 0);
+    } while (action != 4);
     return 0;
 }
 
-void createContact(int *pnumberOfContacts) {
-    *pnumberOfContacts++;
+void getString(char *str, int size) {
+    fgets(str,size,stdin);
+    str[strlen(str)-1] = '\0';
 }
 
-void deleteContact(int *pnumberOfContacts) {
-    *pnumberOfContacts--;
+char* capitalize(char *str) {
+    str[0] = toupper(str[0]);
+    return str;
 }
 
-void displayContactList(int numberOfContacts) {
+void createContact(contact contactList[], int *contactIndex) {
+    char *firstName = contactList[*contactIndex].firstName;
+    char *lastName = contactList[*contactIndex].lastName;
+    char *phoneNumber = contactList[*contactIndex].phoneNumber;    
+    printf("\nFirst Name: ");
+    getString(firstName, 20);
+    capitalize(firstName);
+    printf("Last Name: ");
+    getString(lastName, 20);
+    capitalize(lastName);
+    printf("Phone Number: ");
+    getString(phoneNumber, 15);
+    printf("\n%s %s has been added to your contact list.", firstName, lastName);
+}
 
+void deleteContact(contact contactList[], int * contactIndex) {
+}
+
+void displayContactList(contact contactList[], int * contactIndex) {
 }
